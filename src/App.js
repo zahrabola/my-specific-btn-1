2ab99ddf2152649bfbd04e15c9d3a898c7f517de
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-07 21:14:28 +0100

    debugging challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React, { useState } from "react";
+import "./App.css";
+/*
+Instructions:
+  Can't remove the buttons when clicked , See if you can debug and fix 
+*/
+
+function Parent() {
+  const [children, setChildren] = useState([3, 1, 2]);
+
+  function removeChild(removedId) {
+    setChildren(children.filter((id) => id !== removedId));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Hi there.... </h2>
+      {children.map((id) => (
+        <Child key={id} id={id} remove={() => removeChild(id)} />
+      ))}
     </div>
   );
 }
 
+function Child({ remove, id }) {
+  return (
+    <div>
+      <button onClick={remove}>I'm the child {id}. Click to remove me!</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+   
+  );
+}
+
+
 export default App;
+/*
+
+the debugging part 
+import React, { useState } from "react";
+import "./styles.css";
+/*
+Instructions:
+  Can't remove the buttons when clicked , See if you can debug and fix 
+
+
+function Parent() {
+  const [children, setChildren] = useState([3, 1, 2]);
+
+  function removeChild(removedId) {
+    setChildren(children.filter((id) => id !== removedId));
+  }
+  return (
+    <div>
+      <h2> Hi there.... </h2>
+      {children.map((id) => (
+        <Child key={id} id={id} remove={removeChild} />
+      ))}
+    </div>
+  );
+}
+
+function Child({ remove, id }) {
+  return (
+    <div>
+      <button onClick={remove}>I'm the child {id}. Click to remove me!</button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Parent />
+    </div>
+  );
+}
+*/
